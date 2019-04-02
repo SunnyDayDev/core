7783c860f67d27d56c57d4ab770580e032d538cd
Aleksandr Tcikin <sunnyday.development@gmail.com>
2019-04-02 15:42:55 +0300

    Spaces

--- mvi-spotify/src/main/java/dev/sunnyday/core/mvi/spotify/SubtypeEffectHandlerBuilder.kt
+++ mvi-spotify/src/main/java/dev/sunnyday/core/mvi/spotify/SubtypeEffectHandlerBuilder.kt
@@ -6,6 +6,7 @@ import io.reactivex.Scheduler
 import io.reactivex.functions.Consumer
 
 class SubtypeEffectHandlerBuilder<F, E> {
+
     val builder: RxMobius.SubtypeEffectHandlerBuilder<F, E> = RxMobius.subtypeEffectHandler()
 
     inline fun <reified G : F> addTransformer(
@@ -41,4 +42,5 @@ class SubtypeEffectHandlerBuilder<F, E> {
             apply { builder.withFatalErrorHandler { block(it) } }
 
     fun build(): ObservableTransformer<F, E> = builder.build()
+
 }
