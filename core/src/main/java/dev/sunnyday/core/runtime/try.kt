ced67e73393691abda04a8a7d0387fb94fe53e94
Aleksandr Tcikin <mail@sunnyday.dev>
2019-06-05 06:58:57 +0300

    tryOptional improvements.

--- core/src/main/java/dev/sunnyday/core/runtime/try.kt
+++ core/src/main/java/dev/sunnyday/core/runtime/try.kt
@@ -1,6 +1,5 @@
 package dev.sunnyday.core.runtime
 
-import timber.log.Timber
 import kotlin.contracts.InvocationKind
 import kotlin.contracts.contract
 
@@ -9,7 +8,9 @@ import kotlin.contracts.contract
  * mail: mail@sunnyday.dev
  */
 
-inline fun <T> tryOptional(resolver: ErrorResolverStrategy<T?>? = null, action: () -> T): T? {
+inline fun <T> tryOptional(doOnError: (Throwable) -> Unit = { },
+                           onErrorReturn: T? = null,
+                           action: () -> T): T? {
 
     contract {
         callsInPlace(action, InvocationKind.EXACTLY_ONCE)
@@ -18,37 +19,8 @@ inline fun <T> tryOptional(resolver: ErrorResolverStrategy<T?>? = null, action: 
     return try {
         action()
     } catch (e: Throwable) {
-
-        resolver ?: return null
-        resolve(e, resolver)
-    }
-
-}
-
-sealed class ErrorResolverStrategy<T> {
-
-    abstract val log: Boolean
-
-    data class Value<T>(val value: T, override val log: Boolean = false): ErrorResolverStrategy<T>()
-
-    data class Rethrow<T>(override val log: Boolean = false): ErrorResolverStrategy<T>()
-
-    data class Handle<T>(override val log: Boolean = false, val handle: (Throwable) -> T): ErrorResolverStrategy<T>()
-
-}
-
-@Suppress("NOTHING_TO_INLINE")
-inline fun <T> resolve(error: Throwable,
-                       resolverStrategy: ErrorResolverStrategy<T>): T {
-
-    if (resolverStrategy.log) {
-        Timber.e(error)
-    }
-
-    return when(resolverStrategy) {
-        is ErrorResolverStrategy.Value -> resolverStrategy.value
-        is ErrorResolverStrategy.Handle -> resolverStrategy.handle(error)
-        is ErrorResolverStrategy.Rethrow -> throw error
+        doOnError(e)
+        onErrorReturn
     }
 
 }
