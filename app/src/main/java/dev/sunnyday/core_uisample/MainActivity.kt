9f31492e2df4bc03d7825ab2b9df178a3fb02f40
Aleksandr Tcikin <mail@sunnyday.dev>
2019-10-24 19:34:47 +0300

    Java 8

--- app/src/main/java/dev/sunnyday/core_uisample/MainActivity.kt
+++ app/src/main/java/dev/sunnyday/core_uisample/MainActivity.kt
@@ -1,7 +1,11 @@
 package dev.sunnyday.core_uisample
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
 import dev.sunnyday.core.ui.activity.CoreActivity
 import dev.sunnyday.core.util.intent
 
@@ -11,9 +15,28 @@ class MainActivity : CoreActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        findViewById<View>(R.id.mvvm).setOnClickListener { startActivity(intent<MVVMActivity>()) }
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                add(R.id.content, MainFragment())
+            }
+        }
 
         lifecycle
+
+    }
+
+    class MainFragment() : Fragment() {
+
+        override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? = inflater.inflate(R.layout.fragment_main, container, false)
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            view.findViewById<View>(R.id.mvvm).setOnClickListener { startActivity(intent<MVVMActivity>()) }
+        }
 
     }
 
