16718123501b24532ca764e75a3fa27fbeb706c1
Aleksandr Tcikin <mail@sunnyday.dev>
2019-11-21 07:46:03 +0300

    UI: MVVMDialogFragment based on CoreDialogFragment.

---
+++ ui/src/main/java/dev/sunnyday/core/ui/fragment/CoreDialogFragment.kt
@@ -0,0 +1,81 @@
+package dev.sunnyday.core.ui.fragment
+
+import android.content.Intent
+import androidx.fragment.app.DialogFragment
+import dev.sunnyday.core.ui.listener.*
+
+/**
+ * Created by Aleksandr Tcikin (SunnyDay.Dev) on 2019-03-28.
+ * mail: mail@sunnyday.dev
+ */
+ 
+open class CoreDialogFragment: DialogFragment(),
+    OnBackPressedListener.Registry.Owner,
+    OnActivityResultListener.Registry.Owner,
+    OnRequestPermissionResultListener.Registry.Owner,
+    OnBackPressedListener {
+
+    private val activityOnBackPressedListenerDelegate: OnBackPressedListener = object : OnBackPressedListener {
+
+        override fun onBackPressed(): Boolean {
+            if (isHidden) return false
+            return this@CoreDialogFragment.onBackPressed()
+        }
+
+    }
+
+    override val onBackPressedRegistry: OnBackPressedListener.Registry =
+        DefaultOnBackPressedRegistry()
+
+    override val onActivityResultRegistry: OnActivityResultListener.Registry =
+        DefaultOnActivityResultRegistry()
+
+    override val onRequestPermissionResultRegistry: OnRequestPermissionResultListener.Registry =
+        DefaultOnRequestPermissionResultRegistry()
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (!onActivityResultRegistry.onActivityResult(requestCode, resultCode, data)) {
+            onUnhandledActivityResult(requestCode, resultCode, data)
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (!onRequestPermissionResultRegistry.onRequestPermissionsResult(requestCode, permissions, grantResults)) {
+            onUnhandledRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        (activity as? OnBackPressedListener.Registry.Owner)
+            ?.onBackPressedRegistry
+            ?.add(activityOnBackPressedListenerDelegate)
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+        (activity as? OnBackPressedListener.Registry.Owner)
+            ?.onBackPressedRegistry
+            ?.remove(activityOnBackPressedListenerDelegate)
+
+    }
+
+    override fun onBackPressed(): Boolean =
+        onBackPressedRegistry.onBackPressed() || onUnhandledBackPressed()
+
+    protected open fun onUnhandledBackPressed(): Boolean = false
+
+    protected open fun onUnhandledRequestPermissionsResult(requestCode: Int,
+                                                           permissions: Array<out String>,
+                                                           grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    protected open fun onUnhandledActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+    }
+
+}
