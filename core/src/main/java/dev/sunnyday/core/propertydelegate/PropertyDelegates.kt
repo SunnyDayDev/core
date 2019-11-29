8741e4bb6dc75725536b86012897681b299c5827
Aleksandr Tcikin <mail@sunnyday.dev>
2019-11-29 09:34:20 +0300

    Core: ReferenceWrapper improved. soft/weak delegates improved.

--- core/src/main/java/dev/sunnyday/core/propertydelegate/PropertyDelegates.kt
+++ core/src/main/java/dev/sunnyday/core/propertydelegate/PropertyDelegates.kt
@@ -1,5 +1,6 @@
 package dev.sunnyday.core.propertydelegate
 
+import dev.sunnyday.core.util.Soft
 import dev.sunnyday.core.util.Weak
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
@@ -8,19 +9,36 @@ import kotlin.reflect.KProperty
  * Created by Aleksandr Tcikin (SunnyDay.Dev) on 23.08.2018.
  * mail: mail@sunnyday.dev
  */
- 
-fun <T> weak(creator: () -> T) = WeakProperty(creator)
 
-class WeakProperty<T> constructor(
-        private val creator: () -> T
-): ReadWriteProperty<Any, T> {
+fun <T: Any> weak(creator: (() -> T?)? = null) = WeakProperty(creator)
+
+fun <T: Any> soft(creator: (() -> T?)? = null) = SoftProperty(creator)
+
+class WeakProperty<T: Any> constructor(
+    private val creator: (() -> T?)? = null
+): ReadWriteProperty<Any, T?> {
 
     private val weakValue = Weak<T>()
 
-    override fun getValue(thisRef: Any, property: KProperty<*>): T =
-            weakValue.getOrSet { creator() }
+    override fun getValue(thisRef: Any, property: KProperty<*>): T? =
+        creator?.let(weakValue::getOrSet) ?: weakValue.value
 
-    override fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: T?) {
+        weakValue.value = value
+    }
+
+}
+
+class SoftProperty<T: Any> constructor(
+    private val creator: (() -> T?)? = null
+): ReadWriteProperty<Any, T?> {
+
+    private val weakValue = Soft<T>()
+
+    override fun getValue(thisRef: Any, property: KProperty<*>): T? =
+        creator?.let(weakValue::getOrSet) ?: weakValue.value
+
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: T?) {
         weakValue.value = value
     }
 
